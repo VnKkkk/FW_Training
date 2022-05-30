2d7862d8dcac2ef885b1071fad400974f01f0fea
Sandra Radeva <sandra.radeva@endava.com>
2022-05-30 16:48:06 +0300

    GET all users - status code + status message

--- src/test/java/common/RestActions.java
+++ src/test/java/common/RestActions.java
@@ -3,10 +3,12 @@ package common;
 import dataProviders.ConfigReader;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import org.junit.Assert;
 
 public class RestActions {
 
     private ConfigReader configReader;
+    private Response response;
 
     public RestActions() {
 
@@ -15,8 +17,23 @@ public class RestActions {
 
     public void getResource(String path) {
 
-        Response response = RestAssured.get(configReader.getAPIUrl()+path);
-        System.out.println(response.getStatusCode());
+        Response getResponse = RestAssured.get(configReader.getAPIUrl() + path);
+        this.response = getResponse;
+
     }
 
+    public void returnBody() {
+
+        System.out.println(response.getBody().asString());
+
+    }
+
+    public void returnStatusCode(String statusCode, String statusMessage) {
+
+        String statusLine = response.statusLine();
+        String[] array = statusLine.split("\\s+");
+        System.out.println(array[1] + " " + array[2]);
+        Assert.assertEquals(statusCode, array[1]);
+        Assert.assertEquals(statusMessage, array[2]);
+    }
 }
--- src/test/java/steps/REST/CRUD.java
+++ src/test/java/steps/REST/CRUD.java
@@ -5,30 +5,36 @@ import dataProviders.ConfigReader;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.restassured.response.Response;
 
 public class CRUD {
 
     private ConfigReader configReader;
     private RestActions restActions;
+    private Response response;
 
     public CRUD() {
         configReader = new ConfigReader();
         restActions = new RestActions();
+
     }
 
     @When("I perform a {string} request to {string}")
     public void iPerformARequestTo(String resource, String path) {
 
-        if (resource=="GET") {
+        if (resource.equalsIgnoreCase("GET")) {
             restActions.getResource(path);
         }
     }
 
     @Then("I should receive all users")
     public void iShouldReceiveAllUsers() {
+
+        restActions.returnBody();
     }
 
     @And("Status code {string} with a message {string}")
-    public void statusCodeWithAMessage(String arg0, String arg1) {
+    public void statusCodeWithAMessage(String statusCode, String statusMessage) {
+        restActions.returnStatusCode(statusCode, statusMessage);
     }
 }
